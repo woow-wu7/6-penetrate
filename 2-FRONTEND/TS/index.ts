883072d59306d4ca85499e156c664ac5c5424260
woow_wu7 <woow.wu7@gmail.com>
2022-10-27 18:21:16 +0800

    feat: interview.

--- 2-FRONTEND/TS/index.ts
+++ 2-FRONTEND/TS/index.ts
@@ -5,3 +5,28 @@ type T = "name" | "age";
 type Obj = {
   [k in T]: any;
 };
+
+// 2
+// Exclude
+const str: Exclude<"a" | "1" | "2", "a" | "y" | "z"> = "1";
+
+// 3
+// Omit
+type UserState = {
+  name: string;
+  age: number;
+};
+
+type Person = Omit<UserState, "age">;
+// 等价于
+// type Person {
+//    name: string
+// }
+
+// 4
+// Pick
+type Person2 = Pick<UserState, "age">;
+// 等价于
+// type Person2 {
+//    age: number
+// }
